face92947af9e3f09bda8d7d5f523cc8f22714a5
Angga Dwi Arifandi <namapanjanggua@gmail.com>
2020-09-25 23:28:12 +0700

    Test iOS

---
+++ ios/flutter_web_mobileTests/SampleTest.swift
@@ -0,0 +1,34 @@
+//
+//  SampleTest.swift
+//  flutter_web_mobileTests
+//
+//  Created by Angga Dwi Arifandi on 25/09/20.
+//
+
+import XCTest
+
+class SampleTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let a = true
+        XCTAssertTrue(a)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ ios/flutter_web_mobileTests/flutter_web_mobileTests.swift
@@ -0,0 +1,32 @@
+//
+//  flutter_web_mobileTests.swift
+//  flutter_web_mobileTests
+//
+//  Created by Angga Dwi Arifandi on 25/09/20.
+//
+
+import XCTest
+
+class flutter_web_mobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
